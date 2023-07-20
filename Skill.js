80b2228e6ad280666d14b9ebc0fec1fa84d51d6f
Ajay Kumar <90746723+Ak914014@users.noreply.github.com>
2023-07-20 16:45:58 +0530

    Add files via upload

---
+++ Skill.js
@@ -0,0 +1,10 @@
+var num = 1;
+function cunt(){
+    clicks.innerText=num;
+    num++;
+}
+var clicks = document.getElementById('clicks')
+var btn = document.getElementById('btn');
+btn.addEventListener('click', cunt
+);
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+// var student = {name:"ajay", age:22, roll_no : 2752, address:{city:"knanpur", state:"UP" }};
+
+// console.log(student.address["city"]);
+
+var arr = [40,50,65,7,8,7,87,87,8];
+
+arr.splice(2,1,5,7,9,6,5,);
